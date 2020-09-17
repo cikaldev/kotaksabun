a9611344cf93088bacf5ca6f46b7287f401dc524
Ian Cikal <41433325+cikaldev@users.noreply.github.com>
2020-09-17 11:23:41 +0700

    Create quiz_1.js
    unresolved

---
+++ quiz_1.js
@@ -0,0 +1,9 @@
+/**
+ * @author: iancikal
+ * @title: Teler Bank
+ * @description: Hitung pecahan nominal dalam mata uang rupiah
+ * @link: https://telegra.ph/Quiz1--Teller-Bank-09-17
+ * @publish: 17 Sep 2020
+ **/
+ 
+ 
